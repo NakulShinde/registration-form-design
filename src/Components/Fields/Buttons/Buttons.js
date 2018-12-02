acc4085d451d5ab0870e76d689e5165a10c84add
NakulShinde <nakulshinde.it@gmail.com>
2018-12-03 02:00:52 +0530

    Added submit button enable disable logic & Main container font size update

--- src/Components/Fields/Buttons/Buttons.js
+++ src/Components/Fields/Buttons/Buttons.js
@@ -3,6 +3,6 @@ import styles from "./Buttons.module.scss";
 
 export const SubmitButton = (props) => {
     return <div className={styles.button}>
-        <input type="submit" value="Submit" onClick={props.onClick}></input>
+        <input type="submit" value="Submit" onClick={props.onClick} disabled={!props.isEnabled}></input>
     </div>
 }
