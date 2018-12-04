d16c138226f6a45ba3228bf9fc9963409e94b035
NakulShinde <nakulshinde.it@gmail.com>
2018-12-04 13:16:33 +0530

    TODO comment for config where password strength criteria can be override by password chars criteria

--- src/Components/PasswordCriteria/PasswordCriteria.js
+++ src/Components/PasswordCriteria/PasswordCriteria.js
@@ -5,6 +5,7 @@ import PasswordCriteriaStrength from './PasswordCriteriaStrength'
 const PasswordCriteria = (props) => {
     let content = <PasswordCriteriaStrength passwordStrength={props.passwordStrength}></PasswordCriteriaStrength>
     if (props.charCriteria) {
+        //TODO: passsword strength indicator with char criteria can be enabled with 'charCriteria' props property.
         content = <div>PasswordCharContainer</div>
     }
     return <div>
